7d1df93f7f512105b940bf1f3ea02b32b174525b
Daniele Baschieri <danger.recheng@hotmail.it>
2015-04-10 19:25:42 +0200

    Predisposizione per cambio password ancora da implementare i metodi

--- js/options.js
+++ js/options.js
@@ -33,9 +33,35 @@ function getAllSettings(){
 		var js=JSON.parse(data);
 		for(var i=0;i<js.length;i++){
 			general_settings.push(js[i].key);
-			$("#general").append('<div class="input-group input-group-lg"> <span style="background-color: #d9edf7;" class="input-group-addon" >'+js[i].key+'</span><input type="text" id="'+js[i].key+'" class="form-control" value="'+js[i].value+'" aria-describedby="sizing-addon1"></div>');
+			$("#general").append('<div class="input-group input-group-lg">'+
+									'<span style="background-color: #d9edf7; min-width:250px;" class="input-group-addon" >'+
+										js[i].key+
+									'</span>'+
+									'<input type="text" id="'+js[i].key+'" class="form-control" value="'+js[i].value+'" aria-describedby="sizing-addon1">'+
+								'</div>');
 		}
-		$("#general").append('<button type="button" onclick="saveSettings()" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-floppy-save" aria-hidden="true"></span> SAVE</button>');
+		$("#general").append('<button type="button" onclick="saveSettings()" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-floppy-save" aria-hidden="true"></span> SAVE</button><hr />');
+		
+		$("#general").append('<div class="panel panel-default">'+
+								  '<div class="panel-body basic">'+
+									'<b>Password Manager</b>'+
+								  '</div>'+
+							'</div>'+
+							'<div class="input-group input-group-lg">'+
+								'<span style="background-color: #d9edf7; min-width:250px;" class="input-group-addon" >Old password</span>'+
+								'<input type="password" id="oldPassword" class="form-control" value="" aria-describedby="sizing-addon1">'+
+							 '</div>'+
+							 '<div class="input-group input-group-lg">'+
+								'<span style="background-color: #d9edf7; min-width:250px;" class="input-group-addon" >New password</span>'+
+								'<input type="password" id="oldPassword" class="form-control" value="" aria-describedby="sizing-addon1">'+
+							 '</div>'+
+							 '<div class="input-group input-group-lg">'+
+								'<span style="background-color: #d9edf7; min-width:250px;" class="input-group-addon" >Confirm New password</span>'+
+								'<input type="password" id="oldPassword" class="form-control" value="" aria-describedby="sizing-addon1">'+
+							 '</div>'
+							);
+		$("#general").append('<button type="button" onclick="changePassword()" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> CHANGE</button><hr />');
+		
 	}); 
 
 }
